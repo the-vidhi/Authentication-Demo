4116c1c7573474d9327b3e82cfa23f177278c7bb
Vidhi <74762704+the-vidhi@users.noreply.github.com>
2022-04-03 11:54:42 +0530

    Add files via upload

---
+++ config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    secretKeys: {
+      jwt: process.env.SECRET_TOKEN
+    },
+    swaggerOptions: {
+      swaggerOptions: {
+        defaultModelsExpandDepth: 0,
+      },
+    }
+  }
